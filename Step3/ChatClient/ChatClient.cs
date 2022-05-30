8d95db89f93193888a09d62a1a9053f6f7914001
yoojin <yoojin@odinsoftware.co.kr>
2022-05-30 15:36:14 +0900

    0530 update

---
+++ Step3/ChatClient/ChatClient.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Text;
+
+namespace ChatClient
+{
+    internal class ChatClient
+    {
+        static void Main(string[] args)
+        {
+            var _ipaddress = IPAddress.Parse("127.0.0.1");
+            var _port_no = 5000;
+
+            var _client = new TcpClient();
+            _client.Connect(_ipaddress, _port_no);
+
+            Console.WriteLine("connected to server...");
+            
+            var _stream = _client.GetStream();
+
+            var _receive_thread = new Thread(ReceiveMessage);
+            _receive_thread.Start(_client);
+
+            while (true)
+            {
+                var _message = Console.ReadLine();
+                if (String.IsNullOrEmpty(_message))
+                    break;
+
+                var _buffer = Encoding.UTF8.GetBytes(_message);
+                _stream.Write(_buffer, 0, _buffer.Length);
+            }
+
+            _client.Client.Shutdown(SocketShutdown.Send);
+            _receive_thread.Join(); //쓰레드가 끝나기를 조인하기를 기다림
+
+            _stream.Close();
+
+            Console.WriteLine("Disconnect from Server");
+            Console.ReadKey();
+        }
+
+        static void ReceiveMessage(object o)
+        {
+            var _client = (TcpClient)o;
+
+            var _stream = _client.GetStream();
+            var _buffer = new byte[1024];
+
+            while(true)
+            {
+                var _count = _stream.Read(_buffer, 0, _buffer.Length);
+                if (_count <= 0)
+                    break;
+
+                var _message = Encoding.UTF8.GetString(_buffer, 0, _count);
+                Console.Write(_message);
+            }
+        }
+    }
+}
---
+++ Step3/ChatServer/ChatServer.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace ChatServer
+{
+    internal class ChatServer
+    {
+        static readonly object _locker = new object();
+        static readonly Dictionary<int, TcpClient> _clients = new Dictionary<int, TcpClient>();
+
+
+        static void Main(string[] args)
+        {
+            var _count = 1;
+
+            var _server = new TcpListener(IPAddress.Any, 5000);
+            _server.Start();
+
+            while (true)
+            {
+                var _client = _server.AcceptTcpClient();
+                lock (_locker) { _clients.Add(_count, _client); }
+                Console.WriteLine("client #{0} is connected", _count);
+
+                var _child_thread = new Thread(handle_clients);
+                _child_thread.Start(_count);
+
+                _count++;
+            }
+        }
+
+        public static void handle_clients(object o)
+        {
+            var _sender = (int)o;
+            var _client = (TcpClient)null;
+
+            lock (_locker) { _client = _clients[_sender]; }
+
+            try
+            {
+                while (true)
+                {
+                    var _stream = _client.GetStream();
+                    var _buffer = new byte[1024];
+
+                    var _count = _stream.Read(_buffer, 0, _buffer.Length);
+                    if (_count == 0)
+                        break;
+
+                    var _message = Encoding.UTF8.GetString(_buffer, 0, _count);
+                    Console.WriteLine(_message);
+
+                    broadcast(_message, _sender);
+                }
+
+                _client.Client.Shutdown(SocketShutdown.Both);
+                _client.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"client #{_sender} is error: {ex.Message}");
+            }
+            finally
+            {
+                Console.WriteLine("client #{0} is disconnected", _sender);
+                lock (_locker) { _clients.Remove(_sender); }
+            }
+        }
+
+        public static void broadcast(string data, int sender)
+        {
+            var _packet = Encoding.UTF8.GetBytes(data + Environment.NewLine);
+
+            lock (_locker)
+            {
+                foreach (var _dict in _clients)
+                {
+                    if (_dict.Key == sender)
+                        continue;
+                    // continue문: 아래에 있는 실행해야 하는 문장들을 건너뛰고 다음 반복문 실행.
+                    // break문: 더 이상 반복하지 말고 while, for문 끝냄.
+                    var _client = _dict.Value;
+
+                    var _stream = _client.GetStream();
+                    //TcpClient _client 변수에 GetStream 함수 사용으로 인해 데이터를 보내고 받는데 사용되는 NetworkStream 값을 반환.
+                    _stream.Write(_packet, 0, _packet.Length);
+                }
+            }
+        }
+    }
+}
